40c316e6f784c924c2fde4c9c94884db9466509b
Benjamin Blumenberg <Ben@Benjamins-Pro.lan>
2015-07-29 00:03:05 -0700

    test

--- Operation.java
+++ Operation.java
@@ -4,6 +4,6 @@ public interface Operation{
     
     int getNumArgs();
     void getInfo();
-    String execute(List<Float> args);
+    String execute(List<Float> args); //String to allow for broader response range
     
 }
